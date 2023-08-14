6c7c726179f36ad28f2cc68c82ca313f6b99d893
Jason de Morais <jasoncamposfilho@hotmail.com>
2023-08-14 15:11:19 -0300

    Error package fix, deleted the first Logic exercises

--- aula/Polimorfismo/util/Employee.java
+++ aula/Polimorfismo/util/Employee.java
@@ -1,44 +1,19 @@
-package aula.Polimorfismo.util;
+package Polimorfismo.util;
 
 public class Employee {
     private String name;
-    private Integer hours;
+    private Interger hours;
     private Double valuePerHour;
 
-    public Employee(){
-    }
-
-    public Employee(String name, Integer hours, Double valuePerHour) {
-        this.name = name;
-        this.hours = hours;
-        this.valuePerHour = valuePerHour;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getHours() {
-        return hours;
-    }
-
-    public void setHours(Integer hours) {
-        this.hours = hours;
-    }
-
-    public Double getValuePerHour() {
-        return valuePerHour;
-    }
-
-    public void setValuePerHour(Double valuePerHour) {
-        this.valuePerHour = valuePerHour;
+    public Employee{
     }
     
-    public double payment(){
-        return hours * valuePerHour;
+    public Employee(String name, Interger hours, Double valuePerHour){
+        this.name = name;
+        this.hours = hours;
+        this.valuePerHour = valuePerHour
     }
+
+    
+
 }
--- aula/Polimorfismo/util/OutsourcedEmployee.java
+++ aula/Polimorfismo/util/OutsourcedEmployee.java
@@ -1,30 +1,5 @@
-package aula.Polimorfismo.util;
+package Polimorfismo.util;
 
-public class OutsourcedEmployee extends Employee{
-
-
-    private Double additionalCharge;
-
-    public OutsourcedEmployee(){
-        super();
-    }
-
-    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
-        super(name, hours, valuePerHour);
-        this.additionalCharge = additionalCharge;
-    }
-
-    public Double getAdditionalCharge() {
-        return additionalCharge;
-    }
-
-    public void setAdditionalCharge(Double additionalCharge) {
-        this.additionalCharge = additionalCharge;
-    }
-
-    @Override
-    public double payment(){
-        return super.payment() + additionalCharge * 1.1;
-    }
+public class OutsourcedEmployee {
     
 }
